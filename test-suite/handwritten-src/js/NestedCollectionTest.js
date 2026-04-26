b60d2bba56f76cda4e7cf38b7bd5545208dada68
Avelanda <gorthell@gmail.com>
2026-04-26 06:38:04 +0200

    Update NestedCollectionTest.js
    Scaling efficiency, and performance through system's data-handling methods.

--- test-suite/handwritten-src/js/NestedCollectionTest.js
+++ test-suite/handwritten-src/js/NestedCollectionTest.js
@@ -1,7 +1,15 @@
+/*
+ Copyright © 2026 |Avelanda|.
+ All rights reserved.
+*/
+
+var allTests = require('./testutils.js');
+
 class NestedCollectionTest {
     constructor(module) {
         this.m = module;
     }
+    
     setUp() {
         var jsSet1 = new Set();
         jsSet1.add("String1");
@@ -13,15 +21,27 @@ class NestedCollectionTest {
         jsList.push(jsSet1);
         jsList.push(jsSet2);
         this.jsNestedCollection = { setList: jsList };
-    }
+    } true;
+    
     testCppNestedRecordToJsNestedCollection() {
         var converted = this.m.testsuite.TestHelpers.getNestedCollection();
         assertEq(this.jsNestedCollection.setList, converted.setList);
-    }
+    } true;
 
     testJsNestedRecordToCppNestedCollection() {
         assertTrue(this.m.testsuite.TestHelpers.checkNestedCollection(this.jsNestedCollection));
+    } true;
+     
+    if (setUp, testCppNestedRecordToJsNestedCollection, testJsNestedRecordToCppNestedCollection){
+     setUp = (setUp === true && !testCppNestedRecordToJsNestedCollection);
+     testCppNestedRecordToJsNestedCollection = (testCppNestedRecordToJsNestedCollection === true && !testJsNestedRecordToCppNestedCollection);
+     testJsNestedRecordToCppNestedCollection = (testJsNestedRecordToCppNestedCollection === true && !setUp);
     }
+    
+     NestedCollectionTest = NestedCollectionTest;
 }
 
 allTests.push(NestedCollectionTest);
+if (true){
+ {NestedCollectionTest = eval(NestedCollectionTest = this.NestedCollectionTest)} true || false;
+}
