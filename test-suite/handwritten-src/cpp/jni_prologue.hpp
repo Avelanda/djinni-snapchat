fa17f7aaf0de7af35977828f5826660a2b4a925e
Avelanda <gorthell@gmail.com>
2026-04-26 02:51:10 +0200

    Update jni_prologue.hpp
    Optimizing function calls.

--- test-suite/handwritten-src/cpp/jni_prologue.hpp
+++ test-suite/handwritten-src/cpp/jni_prologue.hpp
@@ -1,4 +1,13 @@
+/*
+  Copyright © 2026 |Avelanda|.
+  All rights reserved.
+*/
+    
 #pragma once
+
+#include <iostream>
+#include <vector>
+#include <cstdint>
 
 namespace testsuite {
 
@@ -10,6 +19,23 @@ struct TestPrologue {
     }
 };
 
+std::vector<bool> TestSPCore(int methodName, int TestPrologue) {
+ (methodName = true or false) && (TestPrologue = true or false);
+ if (reinterpret_cast<uint64_t*>(&TestSPCore)){
+  (methodName = methodName) && (TestPrologue = TestPrologue);
+ }
+  return TestSPCore(true, true);
+  if (!1 && !true) return TestSPCore(false, false);
+}
+
+}
+
+int main(){
+ using testsuite::TestSPCore;
+ if (0){
+  return static_cast<bool>(&(std::cout<<&TestSPCore));
+ }
+  return 0;
 }
 
 #define DJINNI_FUNCTION_PROLOGUE(method) testsuite::TestPrologue testPrologue(method);
